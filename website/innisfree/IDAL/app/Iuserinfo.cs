e1aef1780f181650bc289db0ba4694e9d3a3a149
Rick <rickshy1206@gmail.com>
2013-08-29 01:02:19 +0800

    website version

---
+++ website/innisfree/IDAL/app/Iuserinfo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data;
+namespace Maticsoft.IDAL.app
+{
+	/// <summary>
+	/// 接口层userinfo
+	/// </summary>
+	public interface Iuserinfo
+	{
+		#region  成员方法
+		/// <summary>
+		/// 得到最大ID
+		/// </summary>
+		int GetMaxId();
+		/// <summary>
+		/// 是否存在该记录
+		/// </summary>
+		bool Exists(int ID);
+		/// <summary>
+		/// 增加一条数据
+		/// </summary>
+		bool Add(Maticsoft.Model.app.userinfo model);
+		/// <summary>
+		/// 更新一条数据
+		/// </summary>
+		bool Update(Maticsoft.Model.app.userinfo model);
+		/// <summary>
+		/// 删除一条数据
+		/// </summary>
+		bool Delete(int ID);
+		bool DeleteList(string IDlist );
+		/// <summary>
+		/// 得到一个对象实体
+		/// </summary>
+		Maticsoft.Model.app.userinfo GetModel(int ID);
+		Maticsoft.Model.app.userinfo DataRowToModel(DataRow row);
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		DataSet GetList(string strWhere);
+		/// <summary>
+		/// 根据分页获得数据列表
+		/// </summary>
+		//DataSet GetList(int PageSize,int PageIndex,string strWhere);
+		#endregion  成员方法
+		#region  MethodEx
+
+		#endregion  MethodEx
+	} 
+}
